e65e15422e10e14268dba76f597dcff58cd167f0
yousif aziz <en.yousif.aziz@gmail.com>
2020-04-06 17:32:21 +0200

    add companies token

--- scripts.sh
+++ scripts.sh
@@ -57,3 +57,137 @@ echo ""
 service apache2 restart' >> /usr/local/bin/create_vhost
 chmod +x  /usr/local/bin/create_vhost
 
+
+
+
+
+echo '#!/bin/bash
+
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "Not running as root"
+    exit
+fi
+
+
+
+clear
+echo ""
+echo "#################################################"
+echo "#################################################"
+echo "####                                         ####"
+echo "####                welcome                  ####"
+echo "####          here is Proxy installer        ####"
+echo "####                                         ####"
+echo "####                                JOE AZIZ ####"
+echo "#################################################"
+echo "#################################################"
+echo ""
+
+#clear
+
+read -p "please enter local url: " url
+read -p "please enter proxy url  : " proxy
+echo ""
+echo "create proxy entry in /etc/hosts file ... "
+echo ""
+echo '127.0.0.1	' ${url} >> /etc/hosts;
+
+
+
+echo "create Vhost file in /etc/apache2/sites-available/$url.proxy.conf ... "
+echo ""
+echo "<VirtualHost *:80>
+        ServerName $url
+        ProxyRequests on
+        RequestHeader set Host "$url"
+        ProxyPreserveHost On
+        ProxyPass / $proxy/
+        ProxyPassReverse / $proxy/
+</VirtualHost>"  >> /etc/apache2/sites-available/$url.proxy.conf; 
+
+
+
+
+echo "enabling $url website"
+echo ""
+a2ensite ${url}.proxy
+echo " restart apache server"
+echo ""
+service apache2 restart' >> /usr/local/bin/create_proxy
+chmod +x  /usr/local/bin/create_proxy
+
+
+
+
+echo '#!/bin/bash
+
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "Not running as root"
+    exit
+fi
+
+
+
+clear
+echo ""
+echo "#################################################"
+echo "#################################################"
+echo "####                                         ####"
+echo "####                welcome                  ####"
+echo "####          here is Proxy installer        ####"
+echo "####                                         ####"
+echo "####                                JOE AZIZ ####"
+echo "#################################################"
+echo "#################################################"
+echo ""
+
+#clear
+
+read -p "please enter local url: " url
+read -p "please enter proxy url  : " proxy
+echo ""
+echo "create proxy entry in /etc/hosts file ... "
+echo ""
+echo '127.0.0.1	' ${url} >> /etc/hosts;
+
+
+
+echo "create Vhost file in /etc/apache2/sites-available/$url.proxy.conf ... "
+echo ""
+echo "<Proxy balancer://$url>
+            # Define back-end servers:
+
+            # Server 1
+                BalancerMember $proxy
+
+      </Proxy>
+
+      <VirtualHost *:80>
+            # Apply VH settings as desired
+            # However, configure ProxyPass argument to
+            # use mycluster to balance the load
+        
+        ServerName $url
+
+        ProxyRequests on
+        RequestHeader set Host "$url"
+        ProxyPreserveHost On
+
+
+        ProxyPass / balancer://$url/
+
+      </VirtualHost>"  >> /etc/apache2/sites-available/$url.proxy.conf; 
+
+
+
+
+echo "enabling $url website"
+echo ""
+a2ensite ${url}.proxy
+echo " restart apache server"
+echo ""
+service apache2 restart' >> /usr/local/bin/create_balancer
+chmod +x  /usr/local/bin/create_balancer
+
+
+
